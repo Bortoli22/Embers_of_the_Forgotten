b48926a31a889ba6c286c69425112a5351ee9cc5
Sougat Pradhan <pradhan1@purdue.edu>
2020-10-08 15:09:50 -0700

    Trying to add sprint
    able to detetect shift key, need to translate sprint key detection to player movement

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -5,6 +5,7 @@ extends KinematicBody2D
 export var playerGravity = 9.81
 export var playerSpeed = 400
 export var terminalVelocity = 1500
+export var sprintVelocity = 2500
 export var floatDenominator = 1.3
 
 var playerVelocity = Vector2()
@@ -13,6 +14,7 @@ var currency = 0
 var fsm #finite state machine
 var xPositivity = true
 var crouched = false
+var sprinting = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -33,6 +35,10 @@ func _physics_process(delta):
 		else:
 			crouched = false
 	else:
+		if Input.is_action_pressed("ui_shift"):
+			terminalVelocity = 2000
+		else: 
+			terminalVelocity = 1500
 		if playerVelocity.y < terminalVelocity:
 			playerVelocity.y += delta * playerGravity 
 			if Input.is_action_pressed("ui_down") && playerVelocity.y < terminalVelocity:
@@ -65,6 +71,25 @@ func _inputSequence():
 			playerVelocity.x -= (playerSpeed / 10)
 		else:
 			playerVelocity.x = -playerSpeed
+	#implement sprint to left 
+#	elif Input.is_action_pressed("ui_left") && !Input.is_action_pressed("ui_right"):
+#		fsm.travel("Run_Left")
+#		xPositivity = false
+#		if playerVelocity.x > -playerSpeed:
+#			playerVelocity.x -= (playerSpeed / 10)
+#		else:
+#			playerVelocity.x = -playerSpeed
+	#implement sprint to the right
+	elif Input.is_action_pressed("ui_shift"): # && Input.is_action_pressed("ui_left") && !Input.is_action_pressed("ui_right"):
+		fsm.travel("Run_Left")
+		printerr("hit shift!")
+		sprinting = true
+		playerVelocity.x = playerVelocity.x + 20
+#		xPositivity = false
+#		if playerVelocity.x > -playerSpeed:
+#			playerVelocity.x -= (playerSpeed / 10)
+#		else:
+#			playerVelocity.x = -playerSpeed		
 	else:
 		if !crouched:
 			if xPositivity:
