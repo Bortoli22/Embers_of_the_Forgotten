0cf76bbad4bcebacbfdd701ed1e96f42624e0dc0
Sougat Pradhan <pradhan1@purdue.edu>
2020-12-03 18:51:50 -0800

    Got first enemy variant to move
    got first enemy variant to move, took out move when sees player, now having some bug with enemy speed and the left and right containers of the enemy node interrupt its movement

--- Scenes/Enemies/EnemyVariant1.gd
+++ Scenes/Enemies/EnemyVariant1.gd
@@ -27,35 +27,11 @@ func set_dir(target_dir):
 		next_dir = target_dir
 		next_dir_time = OS.get_ticks_msec() + react_time
 
-func sees_player():
-	var eye_center = get_global_position()
-	var eye_top = eye_center + Vector2(0, -eye_reach)
-	var eye_left = eye_center + Vector2(-eye_reach, 0)
-	var eye_right = eye_center + Vector2(eye_reach, 0)
-
-	var player_pos = Player.get_global_position()
-	var player_extents = Player.get_node("Player Collision").shape.extents - Vector2(1, 1)
-	var top_left = player_pos + Vector2(-player_extents.x, -player_extents.y)
-	var top_right = player_pos + Vector2(player_extents.x, -player_extents.y)
-	var bottom_left = player_pos + Vector2(-player_extents.x, player_extents.y)
-	var bottom_right = player_pos + Vector2(player_extents.x, player_extents.y)
-
-	var space_state = get_world_2d().direct_space_state
-
-	for eye in [eye_center, eye_top, eye_left, eye_right]:
-		for corner in [top_left, top_right, bottom_left, bottom_right]:
-			if (corner - eye).length() > vision:
-				continue
-			var collision = space_state.intersect_ray(eye, corner, [], 1) # collision mask = sum of 2^(collision layers) - e.g 2^0 + 2^3 = 9
-			if collision and collision.collider.name == "Player":
-				return true
-	return false
-
 func _process(delta):
 	#setting correct x coordinate
-	if Player.position.x < position.x - target_player_dist and sees_player():
+	if Player.position.x < position.x - target_player_dist:
 		set_dir(-1)
-	elif Player.position.x > position.x + target_player_dist and sees_player():
+	elif Player.position.x > position.x + target_player_dist :
 		set_dir(1)
 	else:
 		set_dir(0)
@@ -64,13 +40,13 @@ func _process(delta):
 		dir = next_dir
 
 	if OS.get_ticks_msec() > next_jump_time and next_jump_time != -1 and is_on_floor():
-		if Player.position.y < position.y - 64 and sees_player():
+		if Player.position.y < position.y - 64:
 			vel.y = -800
 		next_jump_time = -1
 
 	vel.x = dir * 500
 #setting correct y coordinate 
-	if Player.position.y < position.y - 64 and next_jump_time == -1 and sees_player():
+	if Player.position.y < position.y - 64 and(next_jump_time == -1):
 		next_jump_time = OS.get_ticks_msec() + react_time
 
 	vel.y += grav * delta;
